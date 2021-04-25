934088ccd6103e3a1c454530bb65f7cf2c213d70
Vladislav Bardin <vbardin810@gmail.com>
2021-04-25 22:36:10 +0300

    Update lab template

---
+++ Lab.Shared/Utils/ReportGenerationHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab.Utils.Utils
+{
+    public class ReportGenerationHelper
+    {
+        /// <summary>
+        /// Generate a pool of reports
+        /// </summary>
+        /// <param name="filesAmount"> Amount of file which should be generated </param>
+        public void GenerateLogFiles(int filesAmount)
+        {
+            // Create file writer
+            // Create a cycle and fill file using a method of StreamWriter.WriteLine();
+            // Save the file
+        }
+    }
+}
